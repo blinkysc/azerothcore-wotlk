872b32abf6902e4565c3d53dc716b1c7a51e3a1f
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-09-16 09:38:11 -0500

    Update last-rites-teleporter.sql

--- data/sql/updates/pending_db_world/last-rites-teleporter.sql
+++ data/sql/updates/pending_db_world/last-rites-teleporter.sql
@@ -1,1 +1,2 @@
-
+DELETE FROM `areatrigger_teleport` WHERE `ID` = 5338;
+INSERT INTO `areatrigger_teleport` (`ID`, `Name`, `target_map`, `target_position_x`, `target_position_y`, `target_position_z`, `target_orientation`) VALUES (5338, 'Last Rites Top -> Down', 571, 3733.68, 3563.25, 290.812, 3.665191411972045898);
