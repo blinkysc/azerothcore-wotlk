b176688a2cc9883cdc77bb810ca903c1b86753bf
Gultask <100873791+Gultask@users.noreply.github.com>
2026-02-25 10:02:52 -0300

    Update rev_1772001496427044016.sql

--- data/sql/updates/pending_db_world/rev_1772001496427044016.sql
+++ data/sql/updates/pending_db_world/rev_1772001496427044016.sql
@@ -2,3 +2,4 @@
 -- Entry 67 already guarantees 1 Void Crystal (GroupId=0, 100% chance)
 -- The reference to table 44012 (GroupId=1, 67% chance) added a second Void Crystal
 DELETE FROM `disenchant_loot_template` WHERE `Entry` = 67 AND `Item` = 44012;
+DELETE FROM `reference_loot_template` WHERE (`Entry` = 44012);
