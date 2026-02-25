df225abacb3b89e5e9ddb463696c1415f0da8b8b
blinkysc <37940565+blinkysc@users.noreply.github.com>
2026-02-24 23:15:12 -0600

    Update fix_shaman_t8_electrified_double_proc.sql

--- data/sql/updates/pending_db_world/fix_shaman_t8_electrified_double_proc.sql
+++ data/sql/updates/pending_db_world/fix_shaman_t8_electrified_double_proc.sql
@@ -1,5 +1,2 @@
 -- Remove stale spell_script_names entry for spell_sha_t8_electrified.
--- Spell 64928 (Shaman T8 Elemental 4P Bonus) had two script registrations
--- causing Electrified to proc twice per Lightning Bolt crit.
--- The surviving script is spell_sha_t8_elemental_4p_bonus.
 DELETE FROM `spell_script_names` WHERE `spell_id` = 64928 AND `ScriptName` = 'spell_sha_t8_electrified';
