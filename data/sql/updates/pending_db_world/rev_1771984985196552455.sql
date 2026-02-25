8a4df6d605400132ff29866f888bb7608705bcaf
blinkysc <37940565+blinkysc@users.noreply.github.com>
2026-02-24 20:35:12 -0600

    Fix SQL insert statement for spell_proc

--- data/sql/updates/pending_db_world/rev_1771984985196552455.sql
+++ data/sql/updates/pending_db_world/rev_1771984985196552455.sql
@@ -1,6 +1,3 @@
--- Threat of Thassarian: OH attack should fire even when MH misses/dodges/parries
--- SpellTypeMask 0 = don't filter by spell type (miss/dodge/parry have no damage, so
---   PROC_SPELL_TYPE_DAMAGE won't match - need to allow PROC_SPELL_TYPE_NO_DMG_HEAL too)
 -- HitMask 0x477 = PROC_HIT_NORMAL | PROC_HIT_CRITICAL | PROC_HIT_MISS | PROC_HIT_DODGE | PROC_HIT_PARRY | PROC_HIT_BLOCK | PROC_HIT_ABSORB
 DELETE FROM `spell_proc` WHERE `SpellId` = -65661;
 INSERT INTO `spell_proc` (`SpellId`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `AttributesMask`, `DisableEffectsMask`, `ProcsPerMinute`, `Chance`, `Cooldown`, `Charges`) VALUES
